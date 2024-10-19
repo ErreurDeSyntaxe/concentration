4244730e7184111eb30c3e8d0129150d16d58566
Xavier Bertrand <150564614+ErreurDeSyntaxe@users.noreply.github.com>
2024-10-19 15:35:10 +0800

    Write basic model

--- src/js/model.js
+++ src/js/model.js
@@ -1,17 +1,10 @@
-class Person {
-  constructor(firstName, lastName) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-  }
-  /**
-   * Introduces the Person with their greeting of choice and full name
-   * @param {string} [greeting='Hello']
-   * @returns {string} An introduction with a greeting a full name
-   */
-  greet(greeting = 'Hello') {
-    return `${greeting}. My name is ${this.firstName} ${this.lastName}.`;
-  }
-}
+export const state = {
+  deck: [
+    ['red', 'blood'],
+    ['blue', 'sky'],
+  ],
+};
 
-export const john = new Person('John', 'Doe');
-export const jane = new Person('Jane', 'Doe');
+export const loadDeck = function () {
+  console.log(state.deck);
+};
