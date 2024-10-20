b954377819cf4dda22a64f761d5ca70704c8f123
Xavier Bertrand <150564614+ErreurDeSyntaxe@users.noreply.github.com>
2024-10-20 14:23:06 +0800

    Load deck with user input

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,14 +2,15 @@ import * as model from './model.js'; // import the inner workings
 import view from './view.js'; // import the outer workings
 
 /**
- * Get the user's input
+ * Get the user's input (works if only one textarea is filled)
  * @param {array} _a Throwaway variable (name of the textarea)
  * @param {array} leftWords Textarea content (value)
  * @param {array} _b Throwaway variable (name of the textarea)
  * @param {array} rightWords Textarea content (value)
  */
 const getInputVocab = function ([[_a, leftWords], [_b, rightWords]]) {
-  console.log(leftWords, rightWords);
+  if (rightWords === '') rightWords = leftWords;
+  model.loadDeck(leftWords, rightWords);
 };
 
 /**
--- src/js/model.js
+++ src/js/model.js
@@ -1,10 +1,11 @@
 export const state = {
-  deck: [
-    ['red', 'blood'],
-    ['blue', 'sky'],
-  ],
+  deck: {},
 };
 
-export const loadDeck = function () {
-  console.log(state.deck);
+export const loadDeck = function (left, right) {
+  const leftArr = left.split('\n');
+  const rightArr = right.split('\n');
+  leftArr.forEach((vocab, index) => {
+    state.deck[vocab] = rightArr[index];
+  });
 };
