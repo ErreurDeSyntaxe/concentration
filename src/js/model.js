ca419c9a8cb3fdf5afcbab3a02fc05eed20e32e9
Xavier Bertrand <150564614+ErreurDeSyntaxe@users.noreply.github.com>
2024-10-20 16:57:06 +0800

    Comment function loadDeck

--- src/js/model.js
+++ src/js/model.js
@@ -2,6 +2,11 @@ export const state = {
   deck: {},
 };
 
+/**
+ * Create deck of identical/equivalent cards to play the game
+ * @param {String} left String obtained from the form
+ * @param {String} right String obtained from the form
+ */
 export const loadDeck = function (left, right) {
   const leftArr = left.split('\n');
   const rightArr = right.split('\n');
