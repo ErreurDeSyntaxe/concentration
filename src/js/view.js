4709257fb7dce1ca8f70576f609337441535c354
Xavier Bertrand <150564614+ErreurDeSyntaxe@users.noreply.github.com>
2024-10-23 16:59:56 +0800

    Style card with (& add svg)

--- src/js/view.js
+++ src/js/view.js
@@ -75,6 +75,10 @@ class View {
     });
   }
 
+  _addSVG() {
+    return `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="#FFD00C"><path d="M360-360v-240h240v240H360Zm80-80h80v-80h-80v80Zm-80 320v-80h-80q-33 0-56.5-23.5T200-280v-80h-80v-80h80v-80h-80v-80h80v-80q0-33 23.5-56.5T280-760h80v-80h80v80h80v-80h80v80h80q33 0 56.5 23.5T760-680v80h80v80h-80v80h80v80h-80v80q0 33-23.5 56.5T680-200h-80v80h-80v-80h-80v80h-80Zm320-160v-400H280v400h400ZM480-480Z"/></svg>`;
+  }
+
   /**
    * Render the vocabulary in the form of 'cards' to be flipped by clicking
    * @param {Array} deck The model.state.deck array containing the vocabulary
@@ -87,7 +91,7 @@ class View {
         `
         <div class="card-div">
           <div class="card-face">${curr.word}</div>
-          <div class="card-back">What's Here?</div>
+          <div class="card-back">${this._addSVG()}</div>
         </div>`
       );
     }, '');
